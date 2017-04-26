ebfcc38722949ca8d8d725ecce5cce29be3086d6
taras.yurychko <taras.yurychko@gmail.com>
2017-04-26 09:53:06 +0300

    first commit

---
+++ cont/js/index.js
@@ -0,0 +1,7 @@
+$('document').ready(function() {
+	var options = { videoId: '8luyzJLaj58', start: 3 };
+	$('#wrapper').tubular(options);
+	
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
